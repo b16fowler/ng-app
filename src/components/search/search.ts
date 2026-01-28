ea31205beb1e0044b051db33a28ea62f62fcdc66
Brett Fowler <b16fowler@gmail.com>
2026-01-28 15:44:40 -0500

    Removing api key

--- src/components/search/search.ts
+++ src/components/search/search.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search',
   imports: [],
-  template: ` <button (click)="apiTest()">Search</button> `,
+  template: ` <button>Search</button> `,
   styles: `
     button {
       display: block;
@@ -13,21 +13,19 @@ import { Component } from '@angular/core';
   `,
 })
 export class Search {
-  apiTest = async () => {
-    const url =
-      'https://api.themoviedb.org/3/search/movie?query=Crazy%2C%20Stupid%2C%20Love&include_adult=true&language=en-US&page=1';
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2E2ZjY5M2Q5YmE1OTIyNWNiYzg1MGRlNjdmNWY4NyIsIm5iZiI6MTc2OTUzODA5NC43ODksInN1YiI6IjY5NzkwMjJlYzIwOGNmM2EyNzY4YTM1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zeQfmnBTBYbPxbp3g4BCmqT9ChrbfI0AuBXbgFGJgAg',
-      },
-    };
-
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((json) => console.log(json))
-      .catch((err) => console.error(err));
-  };
+  // apiTest = async () => {
+  //   const url =
+  //     'https://api.themoviedb.org/3/search/movie?query=Crazy%2C%20Stupid%2C%20Love&include_adult=true&language=en-US&page=1';
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       accept: 'application/json',
+  //       Authorization: API_KEY,
+  //     },
+  //   };
+  //   fetch(url, options)
+  //     .then((res) => res.json())
+  //     .then((json) => console.log(json))
+  //     .catch((err) => console.error(err));
+  // };
 }
